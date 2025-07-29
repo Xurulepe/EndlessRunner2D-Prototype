54272d025176f1f90de5b731929d7aeffb33929d
Daniel Costa <xurulepe@gmail.com>
2025-07-29 20:10:53 -0300

    Player jump
    + Player pulando no espaço do teclado e botão sul de controle.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D _rb2D;
+
+    [Header("Jump")]
+    [SerializeField] private float _jumpForce = 5f;
+    [SerializeField] private Transform _groundCheckPos;
+    [SerializeField] private Vector2 _groundCheckSize;
+    [SerializeField] private LayerMask _groundLayer;
+
+    private void Awake()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            _rb2D.linearVelocityY = 0f;
+            _rb2D.AddForceY(_jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapBox(_groundCheckPos.position, _groundCheckSize, 0f, _groundLayer);
+    }
+}
